ba113fbe5c75ff36d100280b34e7791ef364f6eb
deemaJS <deeema@outlook.com>
2015-12-25 16:58:48 +0200

    add 2 app

--- program.js
+++ program.js
@@ -1,1 +1,5 @@
-console.log("HELLO ES6");
+// console.log("HELLO ES6");
+var name = process.argv[2];
+var low = name.toLowerCase();
+console.log(`Hello, ${name}!`);
+console.log(`Your name lowercased is "${low}".`);
