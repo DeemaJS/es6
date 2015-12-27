0ecdecce7455e32ae372cca4a617eb9cac775b17
deemaJS <deeema@outlook.com>
2015-12-27 18:55:05 +0200

    arrow func decision 1

--- program.js
+++ program.js
@@ -7,6 +7,13 @@ console.log(`Your name lowercased is "${low}".`);*/
 
 function name (x) {	return x + 1; } // x => x + 1
 
+var input = process.argv.slice(2);
+var result = input.map(x => x[0])
+				  .reduce( (old, x) => old + x);
+console.log(`[${input}] becomes "${result}"`);
+
+
+
 
 
 
