3eea6725d4823bbd45b5acf13741870edd2b221a
deemaJS <deeema@outlook.com>
2015-12-27 18:43:12 +0200

    arrow func

--- program.js
+++ program.js
@@ -1,5 +1,13 @@
 // console.log("HELLO ES6");
-var name = process.argv[2];
+
+/*var name = process.argv[2];
 var low = name.toLowerCase();
 console.log(`Hello, ${name}!`);
-console.log(`Your name lowercased is "${low}".`);
+console.log(`Your name lowercased is "${low}".`);*/
+
+function name (x) {	return x + 1; } // x => x + 1
+
+
+
+
+
