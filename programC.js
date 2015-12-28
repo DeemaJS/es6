83912e48f796ffb4b29e4a7a1bfed6e09e70e5fc
deemaJS <deeema@outlook.com>
2015-12-28 17:06:00 +0200

    Default args 1

--- programC.js
+++ programC.js
@@ -1,3 +1,8 @@
-module.exports = function average(...args) {
+/*module.exports = function average(...args) {
 	return args.reduce((sum, x) => sum + x, 0) / args.length;
+};*/
+
+
+module.exports = function midpoint(lower = 0, upper = 1) {
+	return (lower + upper) / 2;
 };
